31cc9b020dcf70acf24d5e06da056e49f55361e9
oshi-the-engineer-crypto <sir.oshi.the.engineer@gmail.com>
2026-04-08 21:37:10 +0100

    init commit

---
+++ src/main/java/net/oshi/templatemod/TemplateMod.java
@@ -0,0 +1,60 @@
+package net.oshi.templatemod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.oshi.templatemod.item.ModItems;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TemplateMod.MOD_ID)
+public class TemplateMod {
+    public static final String MOD_ID = "templatemod";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public TemplateMod(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+
+        ModItems.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
+            event.accept(ModItems.OAK_LOG);
+            event.accept(ModItems.BIRCH_LOG);
+        }
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {}
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {}
+    }
+}
---
+++ src/main/java/net/oshi/templatemod/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.oshi.templatemod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.oshi.templatemod.TemplateMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TemplateMod.MOD_ID);
+
+    public static final RegistryObject<Item> OAK_LOG = ITEMS.register("oak_log",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BIRCH_LOG = ITEMS.register("birch_log",
+            () -> new Item(new Item.Properties()));
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
